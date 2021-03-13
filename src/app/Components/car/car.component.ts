dcef81cc5b11394f1ffc0d8fe7f93948255f5f51
Emirhan Demir <emirhndmrr@gmail.com>
2021-03-13 21:49:35 +0300

    .

--- src/app/Components/car/car.component.ts
+++ src/app/Components/car/car.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from 'src/app/models/car';
+import { HttpClient } from '@angular/common/http';
+import { CarResponseModel } from 'src/app/models/carResponseModel';
+import { CarService } from 'src/app/services/car.service';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
-  styleUrls: ['./car.component.css']
+  styleUrls: ['./car.component.css'],
 })
 export class CarComponent implements OnInit {
-
-  cars:Car[]= [];
-  constructor() { }
+  cars: Car[] = [];
+  dataLoaded = false;
+  constructor(private carService : CarService) {}
 
   ngOnInit(): void {
+    this.getCars();
   }
 
+  getCars() {
+
+    this.carService.getCars().subscribe(response=>
+      {
+        this.cars= response.data
+        this.dataLoaded = true;
+      })
+    
+  }
 }
