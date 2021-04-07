b36cc5a739707727be58533de4ffb169babf91f1
Emirhan Demir <emirhndmrr@gmail.com>
2021-04-07 07:10:36 +0300

    Service files have been created✔

--- src/app/services/color.service.ts
+++ src/app/services/color.service.ts
@@ -5,13 +5,13 @@ import { ListResponseModel } from '../models/listResponseModel';
 import { Color } from '../models/color';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ColorService {
-  apiUrl ="https://localhost:44356/api/colors/getall";
-  constructor(private httpClient : HttpClient) { }
+  apiUrl = 'https://localhost:44356/api/colors/getall';
+  constructor(private httpClient: HttpClient) {}
 
-  getColors() : Observable<ListResponseModel<Color>> {
+  getColors(): Observable<ListResponseModel<Color>> {
     return this.httpClient.get<ListResponseModel<Color>>(this.apiUrl);
   }
 }
